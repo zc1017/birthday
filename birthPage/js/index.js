b54cb1566db43acefc8f39afc76cbde03450d839
zhangchuang <1006723342@qq.com>
2023-10-30 16:48:07 +0800

    init

--- birthPage/js/index.js
+++ birthPage/js/index.js
@@ -1,13 +1,24 @@
 $(document).ready(function() {
-
+  if (isMobile()) {
+    $('.zz').css('display','none');
+  } 
   var $clickMe = $('.click-icon'),
-      $card = $('.card');
-  
-  $card.on('click', function() {
+  $card = $('.card');
+$card.on('click', function() {
+  $(this).toggleClass('is-opened');
+  $clickMe.toggleClass('is-hidden');
 
-		$(this).toggleClass('is-opened');
-    $clickMe.toggleClass('is-hidden');
+});
 
-	});
+
+function isMobile() {
+    let flag = navigator.userAgent.match(
+        /(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i
+    );
+    return flag;
+}
+
+
+ 
 
 });
