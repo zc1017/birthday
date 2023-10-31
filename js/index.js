691ec6dc9c61386c54ff4a045fbb51ebab53d5bb
zhangchuang <1006723342@qq.com>
2023-10-31 08:59:42 +0800

    init

--- js/index.js
+++ js/index.js
@@ -1,32 +1,21 @@
 $(document).ready(function() {
-  if (isMobile()) {
+  var sUserAgent = navigator.userAgent;
+  if (sUserAgent.indexOf('Android') > -1 || sUserAgent.indexOf('iPhone') > -1 || sUserAgent.indexOf('iPad') > -1 || sUserAgent.indexOf('iPod') > -1 || sUserAgent.indexOf('Symbian') > -1) {
+    var $clickMe = $('.click-icon'),
+    $card = $('.card');
+  $card.on('click', function() {
+    $(this).toggleClass('is-opened');
+    $clickMe.toggleClass('is-hidden');
+  
+  });
+  var $unsubscribe= $('#unsubscribe');
+  $unsubscribe.on('click',function(event){
+    event.stopPropagation();    //  阻止事件冒泡
+    //
+    $('.bday-card').slideDown(1500);
+    $('.bday-card').css('display','flex');
+  })
+  } else {
     $('.zz').css('display','none');
-  } 
-  var $clickMe = $('.click-icon'),
-  $card = $('.card');
-$card.on('click', function() {
-  $(this).toggleClass('is-opened');
-  $clickMe.toggleClass('is-hidden');
-
-});
-var $unsubscribe= $('#unsubscribe');
-$unsubscribe.on('click',function(event){
-  event.stopPropagation();    //  阻止事件冒泡
-  //
-  $('.bday-card').slideDown(1500);
-  $('.bday-card').css('display','flex');
-})
-
-
-
-function isMobile() {
-    let flag = navigator.userAgent.match(
-        /(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i
-    );
-    return flag;
-}
-
-
- 
-
+  }
 });
