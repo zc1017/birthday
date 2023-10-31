25b79b7f64769cf4e69e81012c913f9ceb00d684
zhangchuang <1006723342@qq.com>
2023-10-31 09:02:15 +0800

    init

--- js/index.js
+++ js/index.js
@@ -1,13 +1,14 @@
 $(document).ready(function() {
   var sUserAgent = navigator.userAgent;
   if (sUserAgent.indexOf('Android') > -1 || sUserAgent.indexOf('iPhone') > -1 || sUserAgent.indexOf('iPad') > -1 || sUserAgent.indexOf('iPod') > -1 || sUserAgent.indexOf('Symbian') > -1) {
+    $('.zz').css('display','none');
     var $clickMe = $('.click-icon'),
     $card = $('.card');
-  $card.on('click', function() {
-    $(this).toggleClass('is-opened');
-    $clickMe.toggleClass('is-hidden');
-  
-  });
+    $card.on('click', function() {
+      $(this).toggleClass('is-opened');
+      $clickMe.toggleClass('is-hidden');
+    
+    });
   var $unsubscribe= $('#unsubscribe');
   $unsubscribe.on('click',function(event){
     event.stopPropagation();    //  阻止事件冒泡
@@ -15,7 +16,5 @@ $(document).ready(function() {
     $('.bday-card').slideDown(1500);
     $('.bday-card').css('display','flex');
   })
-  } else {
-    $('.zz').css('display','none');
-  }
+  } 
 });
